02d09535d6a1fd24d8d81e5e1b76d18e64dd32ca
Tsi <tsilune@gmail.com>
2018-10-04 18:26:22 +0200

    premier jet

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var myNav = document.querySelector('nav');
+var burger = document.querySelector('.burger')
+//Fonction de callback appelait avec un élément
+burger.addEventListener("click", function(e) {
+  myNav.classList.toggle('visible');
+})
